063d48984023c084418c4454a9319876b7d35277
lawriwe <elawriw@gmail.com>
2022-03-30 08:52:29 +0200

    add le cocktail.js

---
+++ cocktails.js
@@ -0,0 +1,25 @@
+ 
+
+
+// pour l'instant sa sert a rien
+function ajax_get_request(callback, url, async = true) {
+	// Instanciation d'un objet XHR
+	var xhr = new XMLHttpRequest();
+
+	// Définition de la fonction à exécuter à chaque changement d'état
+	xhr.onreadystatechange = function(){
+		if (callback && xhr.readyState == XMLHttpRequest.DONE
+				&& (xhr.status == 200 || xhr.status == 0))
+		{
+			// Si une fonction callback est définie + que le serveur a fini son travail
+			// + que le code d'état indique que tout s'est bien passé
+			// => On appelle la fonction callback en passant en paramètre
+			//		les données récupérées sous forme de texte brut
+			callback(xhr.responseText);
+		}
+	};
+
+	// Initialisation de l'objet puis envoi de la requête
+	xhr.open("GET", url, async);
+	xhr.send();
+}
