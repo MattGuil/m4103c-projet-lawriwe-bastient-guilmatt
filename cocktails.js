cbf2e7890e96595f7ebab08d28e570325b19fd6a
lawriwe <elawriw@gmail.com>
2022-04-01 09:56:43 +0200

    decommenté une ligne qui a peut être fixé un bug, je testrais plus tard

--- cocktails.js
+++ cocktails.js
@@ -30,7 +30,7 @@ function recherche(){
 
 // pour avoir plus d'info sur un cocktail d'on on a déja l'id'
 function recherche_suplementaire_cocktails(id){
-	//notIngrePop.classList.remove("show");
+	notIngrePop.classList.remove("show");
 	//let request = document.getElementById("id_cocktail").innerHTML;
 	let EncRequest = encodeURIComponent(id);
 	ajax_get_request(maj_resultat_ingredients,"www.thecocktaildb.com/api/json/v1/1/lookup.php?i="+EncRequest);
